ee0a2044b832eb03138a2b52535c0914192df720
Myke Bates <mbates@thealchemediaproject.com>
2014-12-30 23:09:33 -0600

    Automatic phpDocs for Models
    pretty awesome

--- com.tapmusic.www/app/models/OnlineUser.php
+++ com.tapmusic.www/app/models/OnlineUser.php
@@ -5,6 +5,18 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
+/**
+ * OnlineUser
+ *
+ * @property integer $id
+ * @property string $userID
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\OnlineUser whereId($value) 
+ * @method static \Illuminate\Database\Query\Builder|\OnlineUser whereUserID($value) 
+ * @method static \Illuminate\Database\Query\Builder|\OnlineUser whereCreatedAt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\OnlineUser whereUpdatedAt($value) 
+ */
 class OnlineUser extends Eloquent {
 
 	protected $table = 'online_users';
--- com.tapmusic.www/app/models/Song.php
+++ com.tapmusic.www/app/models/Song.php
@@ -1,5 +1,40 @@
 <?php
 
+/**
+ * Song
+ *
+ * @property integer $id
+ * @property string $spotifyURL
+ * @property string $trackName
+ * @property string $artistName
+ * @property string $albumArt
+ * @property integer $order
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $userID
+ * @property string $userName
+ * @property string $userImage
+ * @property boolean $broadcasting
+ * @property integer $userOnlineID
+ * @property integer $duration
+ * @property integer $start_time
+ * @property-read mixed $song_duration
+ * @method static \Illuminate\Database\Query\Builder|\Song whereId($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereSpotifyURL($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereTrackName($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereArtistName($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereAlbumArt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereOrder($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereCreatedAt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUpdatedAt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUserID($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUserName($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUserImage($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereBroadcasting($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUserOnlineID($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereDuration($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereStartTime($value) 
+ */
 class Song extends Eloquent {
     protected $append = array(
         'song_duration'

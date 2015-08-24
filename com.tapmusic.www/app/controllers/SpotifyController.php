dd57bd0a70d04b94f2df288c9162425f8bec9d67
Tom Rankin <tom@tomrankin.net>
2015-08-24 08:00:27 -0500

    Song don't play because they aren't available in the US market.  Altering the search to only pull back items that are available in the US.  An alternative might be to alter the UI to visually show that a song is not available.

--- com.tapmusic.www/app/controllers/SpotifyController.php
+++ com.tapmusic.www/app/controllers/SpotifyController.php
@@ -17,7 +17,7 @@ class SpotifyController extends BaseController {
     {
         $search = Input::get('search', 'lifestyle');
         $client = new GuzzleHttp\Client();
-        $response = $client->get('https://api.spotify.com/v1/search?q='.$search.'&type=track');
+        $response = $client->get('https://api.spotify.com/v1/search?q='.$search.'&type=track&market=US');
         return $response->getBody();
     }
 
